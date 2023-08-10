b6a8116561e2d112831c9abee501a743db539193
unknown <hrushikesh.1804@gmail.com>
2023-08-10 19:54:26 +0530

    four commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Import BrowserRouter
 import './App.css';
 import Navbar from './Navbar'; // Import your Navbar component
